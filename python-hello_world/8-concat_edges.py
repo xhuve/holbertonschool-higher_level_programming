4748280847bad0d2f30f8adc645d5ff40f3103dc
xhuve <xhuvelialbana@gmail.com>
2024-05-07 10:47:32 +0200

    Fix 7

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.index("object"):str.index("object") + 28] + str[str.index("with"): str.index("with") + 5] + str[:6]
+str = str[39:67] + str[107:112] + str[:6]
 print(str)

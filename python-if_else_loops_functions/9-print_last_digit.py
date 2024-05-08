a3ea8930791226121109e04d200dd3ada6c87b82
xhuve <xhuvelialbana@gmail.com>
2024-05-08 10:13:01 +0200

    Fix return

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,7 @@
 def print_last_digit(number):
     if number < 0:
         print(number * -1 % 10, end="")
+        return (number * -1 % 10)
     else:
         print(number % 10, end="")
+        return (number % 10)

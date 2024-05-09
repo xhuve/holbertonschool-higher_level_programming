c1a5e2f09a8f6e30a0b8f72965cc5230e63ed62f
xhuve <xhuvelialbana@gmail.com>
2024-05-09 11:18:44 +0200

    Fix typo

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
 
     if argc == 1:
-        print("1 argument.")
+        print("1 argument:")
     else:
-        print(argc, "arguments.")
+        print(argc, "arguments:")
 
     for i in range(1, argc + 1):
         print("{}: {}".format(i, sys.argv[i]))

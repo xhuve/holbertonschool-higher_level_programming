2ecae81f37a8757102be3243a5d2622151bacee4
xhuve <xhuvelialbana@gmail.com>
2024-05-09 11:38:21 +0200

    Finish 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    sum = 0
+    argc = len(sys.argv)
+
+    for i in range(1, argc):
+        sum += int(sys.argv[i])
+
+    print(sum)
+    

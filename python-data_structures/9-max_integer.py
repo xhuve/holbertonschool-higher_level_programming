694dc3d9feb1fc1dd78a4c672f8772c01d87dfd2
xhuve <xhuvelialbana@gmail.com>
2024-05-14 13:14:44 +0200

    Finish 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list):
-    maximum = 0
+    maximum = my_list[0]
     if not my_list:
         maximum = None
     for num in my_list:
         if num > maximum:
             maximum = num
-    
+
     return (maximum)

590c4449c877a3197d18446430f1d224635039a2
xhuve <xhuvelialbana@gmail.com>
2024-05-14 10:37:40 +0200

    Fix 6 str format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[i]) - 1):
-            print("{}".format(matrix[i][j]), end=" ")
-        print("{}".format(matrix[i][j + 1]))
+            print("{:d}".format(matrix[i][j]), end=" ")
+        print("{:d}".format(matrix[i][j + 1]))

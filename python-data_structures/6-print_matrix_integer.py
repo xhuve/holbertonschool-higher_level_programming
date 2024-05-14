a58f45a469a3528748212c3552697b87d6545421
xhuve <xhuvelialbana@gmail.com>
2024-05-14 11:08:10 +0200

    Fix 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,11 @@ def print_matrix_integer(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[i]) - 1):
             print("{:d}".format(matrix[i][j]), end=" ")
+        if(len(matrix[i]) == 1):
+            j = -1
         print("{:d}".format(matrix[i][j + 1]))
+
+matrix = [
+    [1]
+]
+print_matrix_integer(matrix)

434d1234c6f781ede689af65b59f7d5d80d74b6e
xhuve <xhuvelialbana@gmail.com>
2024-05-14 10:36:22 +0200

    Finish 6

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,4 +3,3 @@
 def no_c(my_string):
     s = ""
     return s.join([char for char in my_string if char != "c" and char != "C"])
- 
---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i]) - 1):
+            print("{}".format(matrix[i][j]), end=" ")
+        print("{}".format(matrix[i][j + 1]))

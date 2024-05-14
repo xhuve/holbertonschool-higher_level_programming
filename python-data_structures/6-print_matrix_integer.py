2f5c9e4be32f569dfc6d309d14833a4ada8bbc55
xhuve <xhuvelialbana@gmail.com>
2024-05-14 11:10:04 +0200

    Remove test case

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -7,8 +7,3 @@ def print_matrix_integer(matrix):
         if(len(matrix[i]) == 1):
             j = -1
         print("{:d}".format(matrix[i][j + 1]))
-
-matrix = [
-    [1]
-]
-print_matrix_integer(matrix)

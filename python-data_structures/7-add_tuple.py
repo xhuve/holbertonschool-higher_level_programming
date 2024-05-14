2b6de10e43d169f597bef1fbffa634653fae0ded
xhuve <xhuvelialbana@gmail.com>
2024-05-14 12:21:11 +0200

    Remove examples on 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -16,11 +16,3 @@ def add_tuple(tuple_a, tuple_b):
         second.append(0)
 
     return (first[0] + second[0], first[1] + second[1])
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))

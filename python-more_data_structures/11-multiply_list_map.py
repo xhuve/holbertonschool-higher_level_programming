a39cd28f02a8fcf30e9ebbf3a726b01c62a67aec
xhuve <xhuvelialbana@gmail.com>
2024-05-15 15:11:39 +0200

    Remove test case

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -3,8 +3,3 @@
 def multiply_list_map(my_list, number):
     new_list = list(map(lambda x: x * number, my_list))
     return new_list
-
-my_list = [1, 2, 3, 4, 6]
-new_list = multiply_list_map(my_list, 4)
-print(new_list)
-print(my_list)

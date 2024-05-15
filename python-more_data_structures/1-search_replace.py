e58fd8249a5bea07f7985680263792d7981d2c8b
xhuve <xhuvelialbana@gmail.com>
2024-05-15 15:04:04 +0200

    Remove test cases

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 
 def search_replace(my_list, search, replace):
     new_list = [replace if el == search else el for el in my_list]
-    
+
     return new_list
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -24,18 +24,3 @@ def roman_to_int(roman_string):
         i -= 1
 
     return total
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))

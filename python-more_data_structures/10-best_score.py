9c6854b76c02b4ef7989683fae4d443b37eda5be
xhuve <xhuvelialbana@gmail.com>
2024-05-15 14:05:40 +0200

    Remove test case

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,10 +5,3 @@ def best_score(a_dictionary):
         return None
     value = list(a_dictionary.values()).index(max(a_dictionary.values()))
     return list(a_dictionary.keys())[value]
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))

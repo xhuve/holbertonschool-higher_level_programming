e0cebd1069fd41f55e798d410f28fbabc360cdbe
xhuve <xhuvelialbana@gmail.com>
2024-05-15 15:11:02 +0200

    Return new list

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list, number):
-    return list(map(lambda x: x * number, my_list))
+    new_list = list(map(lambda x: x * number, my_list))
+    return new_list
+
+my_list = [1, 2, 3, 4, 6]
+new_list = multiply_list_map(my_list, 4)
+print(new_list)
+print(my_list)

066becc2b9f6fe0a3682d1e798da70797c5c5426
xhuve <xhuvelialbana@gmail.com>
2024-05-15 09:43:51 +0200

    Fix code style n remove test case

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,7 +4,3 @@ def multiple_returns(sentence):
     if not sentence:
         return(0, None)
     return (len(sentence), sentence[0])
-
-sentence = ""
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def max_integer(my_list):
-    maximum = my_list[0]
     if not my_list:
         maximum = None
+    else:
+        maximum = my_list[0]
     for num in my_list:
         if num > maximum:
             maximum = num
-
+    
     return (maximum)

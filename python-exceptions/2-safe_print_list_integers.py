e2008048c3df93a8e1bab4021fe167709ee145ce
xhuve <xhuvelialbana@gmail.com>
2024-05-16 13:54:33 +0200

    Finish 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    print_count = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            print_count += 1
+        except (TypeError, ValueError):
+            pass
+        except IndexError:
+            raise
+
+        i += 1
+    
+    return print_count
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))

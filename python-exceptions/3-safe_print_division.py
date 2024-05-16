b70eeb0d7801643eac2c2fb74a918f93e9e38f5c
xhuve <xhuvelialbana@gmail.com>
2024-05-16 14:15:57 +0200

    Fix 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     res = 0
     try:
         res = a / b
-    except ZeroDivisionError:
+    except ArithmeticError:
         res = None
     finally:
         print("Inside result:", res)

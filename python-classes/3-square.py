c3d74742102087dcc6d21f2ecbbb9a3e72158ec1
xhuve <xhuvelialbana@gmail.com>
2024-05-17 12:16:32 +0200

    Finish 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A square class that represents an actual square"""
+
+
+class Square:
+    """ An empty square class
+
+        Args:
+            size (int): The size of the square
+
+    """
+
+    def __init__(self, size=0):
+        try:
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer""")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        except TypeError:
+            raise
+        except ValueError:
+            raise
+
+    def area(self):
+        """ Method to return the area of the square
+        
+        Return:
+            Pow 2 of the object's size
+        """
+        return self.__size ** 2

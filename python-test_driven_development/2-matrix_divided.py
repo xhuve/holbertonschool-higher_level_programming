96716f749ff6e36426a8a20ec04d91f76ab77854
xhuve <xhuvelialbana@gmail.com>
2024-05-20 16:41:25 +0200

    Finish 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -15,11 +15,11 @@ def matrix_divided(matrix, div):
     string = "matrix must be a matrix (list of lists) of integers/floats"
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-    if type(matrix) != list:
+    if matrix is list:
         raise TypeError(string)
     if not isinstance(matrix[0], list):
         raise TypeError(string)
-    if not all(isinstance(value, int) or isinstance(value, float) for row in matrix for value in row):
+    if not all(isinstance(val, int) or isinstance(val, float) for row in matrix for val in row):
         raise TypeError(string)
     if len(matrix) > 1:
         if all((len(row) != len(matrix[0]) for row in matrix[1:])):

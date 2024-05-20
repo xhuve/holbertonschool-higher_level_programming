e8b622958c0a677c23c85e3a6c00aad524a27c31
xhuve <xhuvelialbana@gmail.com>
2024-05-20 17:04:57 +0200

    Finish 3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Python program to add 2 integers"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ Function to divide values in a matrix
+
+    Args:
+        matrix (list): a list
+        div (int): divider
+
+    Returns:
+        Divided list.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    if last_name:
+        return first_name + " " + last_name
+    else:
+        return first_name

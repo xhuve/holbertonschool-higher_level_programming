ff61391130bf74d711ea3b9f6b99f41cfe0533d2
xhuve <xhuvelialbana@gmail.com>
2024-05-20 13:02:38 +0200

    Fix 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+"""Python program to add 2 integers"""
+
 def add_integer(a, b=98):
     """ Function to add 2 integers
 
-    Parameters:
-    a (int or float): The first number to be added.
-    b (int or float, optional): The second number to be added. Defaults to 98.
+    Args:
+        a (int): an int
+        a (int): another int, defaults to 98
 
     Returns:
-    int: The sum of the two numbers.
+        The sum of the two numbers.
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")

6208251c5ed98411fc476e6a1652f1a2ac48638c
xhuve <xhuvelialbana@gmail.com>
2024-05-20 17:12:19 +0200

    Fix 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -21,6 +21,8 @@ def say_my_name(first_name, last_name=""):
     my_name = "My name is "
 
     if last_name:
-        print(my_name + first_name + " " + last_name)
+        print(my_name + first_name, last_name)
     else:
         print(my_name + first_name)
+
+say_my_name('Bob')

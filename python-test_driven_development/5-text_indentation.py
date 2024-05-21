69ed4de2f37fca809e749b136a7183fa6c5cf776
xhuve <xhuvelialbana@gmail.com>
2024-05-21 10:00:53 +0200

    Finish 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Python program to add 2 integers"""
+
+
+def text_indentation(text):
+    """Function to print a square
+
+        Args:
+            text (str): text that is to be printed
+
+        Return:
+            Returns a indented text
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    new_text = text.split()
+    for word in new_text:
+        print(word, end="")
+        if word == new_text[-1]:
+            break
+        if '.' in word or ',' in word or '?' in word:
+            print()
+        else:
+            print("", end=" ")

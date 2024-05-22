becf9c2ab97a9f756a2a2c332ff69c76a8726a2d
xhuve <xhuvelialbana@gmail.com>
2024-05-22 21:20:35 +0200

    Finish 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -16,14 +16,3 @@ class MyList(list):
                 List containing variables and attributes
         """
         print(sorted(my_list))
-
-
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)

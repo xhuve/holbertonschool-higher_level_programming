46d3433b92e3723d7baf425c80764faaeb93e365
xhuve <xhuvelialbana@gmail.com>
2024-05-22 23:29:06 +0200

    Fix pcs

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Programs that i am using to learn inheritance more in depth"""
 
+
 def is_same_class(obj, a_class):
-    """Function exactly like type
+    """Function exactly like is
 
         Return:
-            What type would return    
+            What is would return
     """
-    return type(obj) == a_class
+    return obj is a_class
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Programs that i am using to learn inheritance more in depth"""
 
+
 def is_kind_of_class(obj, a_class):
     """Function exactly like isinstance
 
         Return:
-            What isinstance would return    
+            What isinstance would return
     """
     return isinstance(obj, a_class)

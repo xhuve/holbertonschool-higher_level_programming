4644abc2ba11b4b7e4b0248dfbd9d1531ee08b10
xhuve <xhuvelialbana@gmail.com>
2024-05-22 10:33:23 +0200

    Finish 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Documentation"""
+
+
+class Rectangle:
+    """ An empty rectangle class """
+    
+    def __init__(self, width=0, heigh=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height

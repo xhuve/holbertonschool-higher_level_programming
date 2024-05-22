759ca87f86ca4176048c607492c70460fd9efafc
xhuve <xhuvelialbana@gmail.com>
2024-05-22 23:24:25 +0200

    Finish 2

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Programs that i am using to learn inheritance more in depth"""
 
+
 class MyList(list):
     """A list object, in essence a list"""
-
 
     def print_sorted(self):
         """A function that prints an sorted list
---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Programs that i am using to learn inheritance more in depth"""
+
+def is_same_class(obj, a_class):
+    return type(obj) == a_class
+
+a = 1
+if is_same_class(a, int):
+    print("{} is an instance of the class {}".format(a, int.__name__))
+if is_same_class(a, float):
+    print("{} is an instance of the class {}".format(a, float.__name__))
+if is_same_class(a, object):
+    print("{} is an instance of the class {}".format(a, object.__name__))

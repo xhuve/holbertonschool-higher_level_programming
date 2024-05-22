d0109c620b4df07865aaa75eb8e283f1e7ada181
xhuve <xhuvelialbana@gmail.com>
2024-05-22 23:42:47 +0200

    Fix 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Programs that i am using to learn inheritance more in depth"""
+
+
+def inherits_from(obj, a_class):
+    """Function exactly like isinstance
+
+        Return:
+            What isinstance would return
+    """
+    return issubclass(type(obj), a_class)

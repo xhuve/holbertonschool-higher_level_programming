8e4015fb95afb6ff96ef07479de775b80f8b1380
xhuve <xhuvelialbana@gmail.com>
2024-05-22 23:27:41 +0200

    Fix docs

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,4 +2,9 @@
 """Programs that i am using to learn inheritance more in depth"""
 
 def is_same_class(obj, a_class):
+    """Function exactly like type
+
+        Return:
+            What type would return    
+    """
     return type(obj) == a_class
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,4 +2,9 @@
 """Programs that i am using to learn inheritance more in depth"""
 
 def is_kind_of_class(obj, a_class):
+    """Function exactly like isinstance
+
+        Return:
+            What isinstance would return    
+    """
     return isinstance(obj, a_class)

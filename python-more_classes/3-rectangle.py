e5bd43889280f7b12fb3bfceec4fa8e8ab6508b1
xhuve <xhuvelialbana@gmail.com>
2024-05-22 10:52:40 +0200

    Finish 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -40,3 +40,25 @@ class Rectangle:
 
     def area(self):
         return self.height * self.width
+    
+    def __str__(self):
+        image = ""
+        if self.height == 0 or self.width == 0:
+            return image
+        for _ in range(self.height):
+            image += "#" * self.width
+            image += "\n"
+        return image
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--")
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle)
+print(repr(my_rectangle))

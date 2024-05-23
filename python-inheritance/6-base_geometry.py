d17bed14560d9319a51f3e5b9faef0fbcb33c37b
xhuve <xhuvelialbana@gmail.com>
2024-05-23 10:58:32 +0200

    Finish 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Programs that i am using to learn inheritance more in depth"""
+
+
+class BaseGeometry(object):
+    """A geometry class containing simple geometrical operations"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")

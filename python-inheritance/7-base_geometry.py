04157a98c11e676ff126d396927f95d380998333
xhuve <xhuvelialbana@gmail.com>
2024-05-23 11:22:19 +0200

    Finish 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Programs that i am using to learn inheritance more in depth"""
+
+
+class BaseGeometry(object):
+    """A geometry class containing simple geometrical operations"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")

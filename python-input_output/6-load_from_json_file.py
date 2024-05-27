50ad4b00ed6d6ef9498e49409a3a9fa53ca2aa76
xhuve <xhuvelialbana@gmail.com>
2024-05-27 15:17:01 +0200

    Fix 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """Get json from file"""
-    with open(filename) as f:
+    with open(filename, encoding='utf-8') as f:
         return json.load(f)

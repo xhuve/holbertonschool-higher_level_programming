57c423bb64302f1c83dfa155b3bba27c81d6423a
xhuve <xhuvelialbana@gmail.com>
2024-05-27 18:35:13 +0200

    Finish 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Programs to learn IO in python"""
+
+
+class Student():
+    def __init__(self, first_name, last_name, age):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__age = age
+
+    def to_json(self, attrs=None):
+        dictionary = {
+            "first_name": self.__first_name,
+            "last_name": self.__last_name,
+            "age": self.__age
+        }
+
+        if attrs == None:
+            return dictionary
+
+        wanted_dict = {}
+        for attr in attrs:
+            if attr in dictionary:
+                wanted_dict[attr] = dictionary[attr]
+        return wanted_dict

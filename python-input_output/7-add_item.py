79d5a7cdb6783f9bbd1fe49ae01e7d16b227a2f8
xhuve <xhuvelialbana@gmail.com>
2024-05-27 17:50:14 +0200

    Finish 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Programs to learn IO in python"""
+from sys import argv
+from os.path import exists 
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+my_list = []
+if exists("add_items.json"):
+    my_list = load_from_json_file("add_items.json")
+for arg in argv[1:]:
+    my_list.append(arg)
+save_to_json_file(my_list, "add_items.json")

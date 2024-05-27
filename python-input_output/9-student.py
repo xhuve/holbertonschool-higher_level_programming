516c1907cdcfdee6d5bdf77c67456943f636ef57
xhuve <xhuvelialbana@gmail.com>
2024-05-27 18:22:00 +0200

    Fix 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -14,14 +14,3 @@ class Student():
             "last_name": self.__last_name,
             "age": self.__age
         }
-
-students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
-
-for student in students:
-    j_student = student.to_json()
-    print(type(j_student))
-    print(j_student['first_name'])
-    print(type(j_student['first_name']))
-    print(j_student['age'])
-    print(type(j_student['age']))
-

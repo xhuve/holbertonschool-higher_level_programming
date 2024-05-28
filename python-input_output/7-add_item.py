0d6a071fb4ddc4879f55fce29b87c8de247ea244
xhuve <xhuvelialbana@gmail.com>
2024-05-28 11:10:31 +0200

    Fix 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,8 +7,8 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 if __name__ == '__main__':
     my_list = []
-    if exists("add_items.json"):
-        my_list = load_from_json_file("add_items.json")
+    if exists("add_item.json"):
+        my_list = load_from_json_file("add_item.json")
     for arg in argv[1:]:
         my_list.append(arg)
-    save_to_json_file(my_list, "add_items.json")
+    save_to_json_file(my_list, "add_item.json")

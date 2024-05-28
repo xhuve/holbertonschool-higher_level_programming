90e99252904f0b03fcb1d23c6bcb6c9efd0a7fed
xhuve <xhuvelialbana@gmail.com>
2024-05-28 11:08:17 +0200

    Fix 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,10 +5,10 @@ from os.path import exists
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-
-my_list = []
-if exists("add_items.json"):
-    my_list = load_from_json_file("add_items.json")
-for arg in argv[1:]:
-    my_list.append(arg)
-save_to_json_file(my_list, "add_items.json")
+if __name__ == '__main__':
+    my_list = []
+    if exists("add_items.json"):
+        my_list = load_from_json_file("add_items.json")
+    for arg in argv[1:]:
+        my_list.append(arg)
+    save_to_json_file(my_list, "add_items.json")

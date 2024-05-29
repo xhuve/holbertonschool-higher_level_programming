c17bbbabbe4c1c8372752322aa9bb103bcc483d7
xhuve <xhuvelialbana@gmail.com>
2024-05-29 09:23:21 +0200

    Fix 0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -2,15 +2,10 @@
 import json
 
 def serialize_and_save_to_file(data, filename):
-    try:
-        with open(filename, "w") as f:
-            return json.dump(data, f)
-    except Exception:
-        return None
+    with open(filename, "w") as f:
+        return json.dump(data, f)
+
 
 def load_and_deserialize(filename):
-    try:
-        with open(filename, "r") as f:
-            return json.load(f)
-    except Exception:
-        return None
+    with open(filename, "r") as f:
+        return json.load(f)

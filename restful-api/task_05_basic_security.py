7eb614f1511dce003503859b5b3a155c1a285535
xhuve <xhuvelialbana@gmail.com>
2024-05-30 18:52:16 +0200

    Add jwt functionality

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -2,7 +2,7 @@
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
@@ -24,6 +24,25 @@ def verify_password(username, password):
 @auth.login_required
 def basicRoute():
     return "Basic Auth: Access Granted"
+
+@app.post("/login")
+def loginRoute():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    if users.get(username) and check_password_hash(users[username].get("password"), password):
+        return {"access_token": create_access_token(identity=username)}
+
+@app.get("/jwt-protected")
+@jwt_required()
+def jwtRoute():
+    curr_user = get_jwt_identity()
+    return jsonify({"logged_in": curr_user})
+
+
+@auth.error_handler
+def unauthorized():
+    return jsonify({"message": "Unauthorized"}), 401
 
 if __name__ == "__main__": 
     app.run()

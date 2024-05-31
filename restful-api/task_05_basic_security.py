35af207b0c7932eb75f8a1e00685266f6ea87e3f
xhuve <xhuvelialbana@gmail.com>
2024-05-31 12:21:58 +0200

    Fix 5

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -5,8 +5,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
+
+app.config["SECRET_KEY"] = 'testkey'
 app.config["JWT_SECRET_KEY"] = "secret-key"
+
+auth = HTTPBasicAuth()
 jwt = JWTManager(app)
 
 
@@ -50,11 +53,27 @@ def adminRoute():
     if user and users[user].get("role") == "admin":
         return "Admin Access: Granted"
     else:
-        return "403 Forbidden", 403
+        return jsonify("403 Forbidden"), 403
 
-@auth.error_handler
-def unauthorized():
-    return jsonify({"message": "Unauthorized"}), 401
+@jwt.unauthorized_loader
+def handle_unauthorized_error(err):
+    return jsonify({"error": "Missing or invalid token"}), 401
+
+@jwt.invalid_token_loader
+def handle_invalid_token_error(err):
+    return jsonify({"error": "Invalid token"}), 401
+
+@jwt.expired_token_loader
+def handle_expired_token_error(err):
+    return jsonify({"error": "Token has expired"}), 401
+
+@jwt.revoked_token_loader
+def handle_revoked_token_error(err):
+    return jsonify({"error": "Token has been revoked"}), 401
+
+@jwt.needs_fresh_token_loader
+def handle_needs_fresh_token_error(err):
+    return jsonify({"error": "Fresh token required"}), 401
 
 if __name__ == "__main__": 
     app.run()

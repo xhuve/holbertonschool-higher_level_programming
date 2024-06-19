0074e85f855460ffad15043b4f25eb98a9d2dde1
xhuve <xhuvelialbana@gmail.com>
2024-06-19 20:17:32 +0200

    Fix 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
-)
+);
 
-INSERT INTO IF EXISTS second_table VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8)
+INSERT INTO IF EXISTS second_table VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);

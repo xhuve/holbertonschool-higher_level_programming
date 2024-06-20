c594d4b45260a693704e17a5aa12445eaa09bef2
xhuve <xhuvelialbana@gmail.com>
2024-06-20 18:25:29 +0200

    Finish 2

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- List privileges
 
 CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' WITH GRANT OPTION;

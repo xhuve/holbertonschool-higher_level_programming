c2aae076f913ce36818879536335ea1e2a4dc77e
xhuve <xhuvelialbana@gmail.com>
2024-06-20 20:59:16 +0200

    finish 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- List privileges
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL FOREIGN KEY REFERENCES states(id)
+    name VARCHAR(256) NOT NULL
+)
+

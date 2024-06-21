1f3e783cd2d643870ced2e5b65fd1c9db67eb208
xhuve <xhuvelialbana@gmail.com>
2024-06-21 19:26:16 +0200

    fix 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.a
 
 cur = db.cursor()
 
-cur.execute("SELECT * FROM messages")
+cur.execute("SELECT * FROM states")
 
 rows = cur.fetchall()
 

d9f1bc96d2bac48accd6e97a3672e25d9bf4fea1
xhuve <xhuvelialbana@gmail.com>
2024-06-21 19:25:35 +0200

    Fix 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,14 +2,14 @@ import sys
 import MySQLdb
 
 
-db = MySQLdb.connect(host='localhost:3306', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[2])
+db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
 cur = db.cursor()
 
-cur.execute("SELECT * FROM states")
+cur.execute("SELECT * FROM messages")
 
 rows = cur.fetchall()
 
 for row in rows:
-    for col in row:
-        print(col)
+    print(row)
+    

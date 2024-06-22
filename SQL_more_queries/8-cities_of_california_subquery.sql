957e85da84c82b4c38b7fdd781fab488cd54665e
xhuve <xhuvelialbana@gmail.com>
2024-06-22 11:48:14 +0200

    Fix 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- List privileges
 
 SELECT * FROM cities 
-WHERE state_id = (SELECT id FROM states WHERE name = "California")
+WHERE state_id = ((SELECT id FROM states WHERE name = "California"))
 ORDER BY id;
 

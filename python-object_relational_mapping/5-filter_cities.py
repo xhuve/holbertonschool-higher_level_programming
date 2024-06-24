65dc6506a165353dc7271f0c60a2608a41bb7781
xhuve <xhuvelialbana@gmail.com>
2024-06-24 14:52:49 +0200

    Fix

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
 
     curr.execute("""SELECT cities.name FROM cities
                  INNER JOIN states ON cities.state_id = states.id
-                 ORDER BY cities.id
-                 HAVING states.name = %s""", (argv[4],))
+                 WHERE states.name = %s
+                 ORDER BY cities.id""", (argv[4],))
 
     rows = curr.fetchall()
 

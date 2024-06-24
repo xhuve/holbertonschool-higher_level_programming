02d51834eb58dc907be2a0f8ffba91f4bbb746e6
xhuve <xhuvelialbana@gmail.com>
2024-06-24 12:48:21 +0200

    Fix 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Learning SQL"""
 
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
@@ -7,6 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
+    """Learning SQL"""
     __tablename__ = 'states'
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, unique=True)

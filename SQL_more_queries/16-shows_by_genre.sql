59c546d08a2a6b8edf2942fead9bda16d3ddbe59
xhuve <xhuvelialbana@gmail.com>
2024-06-24 10:02:51 +0200

    Fix 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -2,7 +2,7 @@
 
 SELECT tv_shows.title AS title, tv_genres.name 
 FROM tv_genres
-	LEFT JOIN tv_show_genres
+	RIGHT JOIN tv_show_genres
 	ON tv_show_genres.genre_id = tv_genres.id
 		LEFT JOIN tv_shows
 		ON tv_shows.id = tv_show_genres.show_id

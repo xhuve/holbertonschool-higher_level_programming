423e3fec3e08bc11317b190cdd21be79ea5014d0
xhuve <xhuvelialbana@gmail.com>
2024-06-24 09:09:59 +0200

    Finish 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+from sqlalchemy import sa, String
+from sqlalchemy.orm import Mapped, mapped_colum, declarative_base
+
+db = sa.create_engine("mysql://localhost:3306")
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id: Mapped[int] = mapped_colum(primay_key=True, autoincrement=True, unique=True)
+    name: Mapped[str] = mapped_colum(String(128), nullable=False) 

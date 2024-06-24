1c2a635c71f5a1db62418be73b1a8228206a23fe
xhuve <xhuvelialbana@gmail.com>
2024-06-24 20:29:21 +0200

    Finish 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Learning SQLAlchemy"""
+
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+        )
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        query = session.query(State).order_by(State.id).filter(State.name.ilike("%n%"))
+        for row in query:
+            print(f"{row.id}: {row.name}")

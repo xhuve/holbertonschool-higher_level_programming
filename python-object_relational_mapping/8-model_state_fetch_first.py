ccc057ae0b57e043aaac05df0719df6e5e404282
xhuve <xhuvelialbana@gmail.com>
2024-06-24 17:50:45 +0200

    Finish 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Learning SQLAlchemy"""
+
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        for state in session.query(State).order_by(State.id).first():
+            print(f"{state.id}: {state.name}")

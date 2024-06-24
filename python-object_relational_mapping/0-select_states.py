c2cabb919cebd1356f4c9c418daa2b51ba016938
Percy <62884604+xhuve@users.noreply.github.com>
2024-06-24 11:15:41 +0000

    Fix 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
 
 
     db = MySQLdb.connect(host='localhost',
-						port=3306,
-						user=sys.argv[1],
-						passwd=sys.argv[2],
-                    	db=sys.argv[3])
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3])
 
     cur = db.cursor()
 

564b6b1557131543cd401468237b30a15af7a914
xhuve <xhuvelialbana@gmail.com>
2024-06-24 09:48:35 +0200

    Fix 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -5,5 +5,5 @@ SELECT tv_shows.title AS title, tv_genres.name FROM tv_genres
 	ON tv_show_genres.genre_id = tv_genres.id
 		RIGHT JOIN tv_shows
 		ON tv_shows.id = tv_show_genres.show_id
-		GROUP BY title
-		ORDER BY title;
+		ORDER BY title
+		GROUP BY title;

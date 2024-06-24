b8d231bb647d9de67e9f2014090c9ddf2619730e
xhuve <xhuvelialbana@gmail.com>
2024-06-24 10:51:27 +0200

    Fix 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""SQL learning"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    """SQL learning"""
+
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()

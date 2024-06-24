fa4a3a45d78e77291eafe603896acd1655f551af
xhuve <xhuvelialbana@gmail.com>
2024-06-24 10:43:40 +0200

    Finish 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """SQL learning"""
-import sys
-import MySQLdb
+
 
 
 if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()

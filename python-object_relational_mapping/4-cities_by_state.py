d885e61c73946c89ab4fc2dab899a1649a4cb41a
xhuve <xhuvelialbana@gmail.com>
2024-06-24 11:18:31 +0200

    Finish 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""SQL learning"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    """SQL Learning"""
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    curr = db.cursor()
+
+    rows = curr.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states WHERE cities.state_id = states.id ORDER BY cities.id")
+
+    for row in rows:
+        print(row)
+
+    curr.close()
+    db.close()

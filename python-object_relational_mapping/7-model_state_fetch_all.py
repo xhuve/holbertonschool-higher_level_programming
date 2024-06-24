5969842b0f76fe84ea280e723b78c7d0b0473184
xhuve <xhuvelialbana@gmail.com>
2024-06-24 17:49:44 +0200

    fix

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,20 +5,11 @@ if __name__ == '__main__':
     from model_state import Base, State
     from sys import argv
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.orm import Session
 
     engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
     Base.metadata.create_all(engine)
 
-<<<<<<< HEAD
-    Session = sessionmaker(engine)
-    session = Session()
-
-    for state in session.query(State).order_by(State.id).all():
-        print(f"{state.id}: {state.name}")
-=======
     with Session(engine) as session:
         for state in session.query(State).order_by(State.id).all():
             print(f"{state.id}: {state.name}")
-
->>>>>>> parent of 0518b3d (Fix 7)

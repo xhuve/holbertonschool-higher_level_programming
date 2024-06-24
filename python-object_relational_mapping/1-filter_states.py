23fc921dac89e390cdb7451a3e95c52ca1a9bb56
xhuve <xhuvelialbana@gmail.com>
2024-06-24 14:08:42 +0200

    fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,14 +8,14 @@ if __name__ == '__main__':
     """SQL Learning"""
 
     db = MySQLdb.connect(host='localhost',
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3])
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     curr = db.cursor()
 
-    curr.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    curr.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
 
     rows = curr.fetchall()
 

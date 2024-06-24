a8ff9562726b921c00dfc35991830c790df602d8
xhuve <xhuvelialbana@gmail.com>
2024-06-24 10:10:18 +0200

    fix 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres
     LEFT JOIN tv_show_genres
-    ON tv_show_genres.show_id = tv_genres.id
+    ON tv_show_genres.genre_id = tv_genres.id
     GROUP BY genre
     HAVING number_of_shows > 0
     ORDER BY number_of_shows DESC;

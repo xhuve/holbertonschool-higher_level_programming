cb08a5a9dfff00bb1810277501a713cf0577bd5c
xhuve <xhuvelialbana@gmail.com>
2024-06-24 12:22:48 +0200

    Fix all

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -11,11 +11,10 @@ if __name__ == '__main__':
 
     curr = db.cursor()
 
-    rows = curr.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    rows = curr.execute("SELECT cities.name FROM cities INNER JOIN states WHERE cities.state_id = states.id ORDER BY cities.id HAVING states.name = '{}'".format(sys.argv[4]))
 
     for row in rows:
         print(row)
 
     curr.close()
     db.close()
-

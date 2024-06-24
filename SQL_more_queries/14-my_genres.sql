2e8f89c1477251424cfa6124276360bb191157fb
xhuve <xhuvelialbana@gmail.com>
2024-06-24 10:06:49 +0200

    Fix 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- show genre id
 
-SELECT tv_genres.name AS genre FROM tv_genres
+SELECT tv_genres.name AS name FROM tv_genres
 	LEFT JOIN tv_show_genres
 	ON tv_show_genres.genre_id = tv_genres.id
 		LEFT JOIN tv_shows

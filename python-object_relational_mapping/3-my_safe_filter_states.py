6d55eee75938ef95ef0db009a754130d00b46de8
xhuve <xhuvelialbana@gmail.com>
2024-06-24 14:46:38 +0200

    Fix

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     curr = db.cursor()
 
-    s = """SELECT * FROM states WHERE name LIKE BINARY %s' ORDER BY id""", (argv[4])
+    s = """SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id""", argv[4]
 
     curr.execute(s)
 

9c62104eedc9c14098a6694dcc38111a5ac049fc
xhuve <xhuvelialbana@gmail.com>
2024-06-24 16:28:05 +0200

    Fix

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,11 +3,11 @@
 
 from model_state import Base, State
 from sys import argv
-from sqlalchemy import Select
+from sqlalchemy import Select, create_engine
 
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     engine = create_engine("mysql://%s:%s@localhost:3306/%s", (argv[1], argv[2], argv[3]))
 
     select_query = Select(State).order_by(State.c.id)

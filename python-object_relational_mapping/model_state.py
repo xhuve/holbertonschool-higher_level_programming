964e6a17d9a8e95b7e705a4c18eeb5c36306e43d
xhuve <xhuvelialbana@gmail.com>
2024-06-24 09:17:44 +0200

    Fix 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,8 +2,6 @@ from sqlalchemy import sa, String
 from sqlalchemy.orm import Mapped, mapped_colum
 from sqlalchemy.ext.declarative import declarative_base
 
-db = sa.create_engine("mysql://root:root@localhost:3306/hbtn_0e_6_usa")
-
 Base = declarative_base()
 
 class State(Base):

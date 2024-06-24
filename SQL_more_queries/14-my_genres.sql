d4c3a9a5d7f2607d12b5385551108f98f915bdbc
xhuve <xhuvelialbana@gmail.com>
2024-06-24 10:04:51 +0200

    Fix 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -6,4 +6,5 @@ SELECT tv_genres.name AS genre FROM tv_genres
 		LEFT JOIN tv_shows
 		ON tv_shows.id = tv_show_genres.show_id
 		WHERE tv_shows.title = 'Dexter'
-		GROUP BY genre;
+		GROUP BY genre
+		ORDER BY genre;

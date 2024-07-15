38f196177d9629db6636564006b8144ee2578b37
xhuve <xhuvelialbana@gmail.com>
2024-07-15 12:51:14 +0200

    Finish

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,30 @@
+
+def generate_invitations(template, attendees):
+    if not template:
+        print("Template is empty, no output files generated.")
+        return
+    
+    if not attendees:
+        print("No data provided, no output files generated.")
+        return
+
+    if not isinstance(template, str):
+        print("template should be a string")
+        return
+    
+    if not all([isinstance(value, dict) for value in attendees]):
+        print("attendees should be a list of dict's")
+        return
+    
+    for i, attendee in enumerate(attendees):
+        personal_template = template
+        for attr, value in attendee.items():
+            if not isinstance(value, str):
+                value = attr + ": N/A"
+            personal_template = personal_template.replace('{' + attr + '}', value)
+        with open('output_' + str(i + 1) + ".txt", 'w', encoding="utf-8") as f:
+            f.write(personal_template)
+
+    
+
+

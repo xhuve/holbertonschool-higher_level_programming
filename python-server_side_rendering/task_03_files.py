e5f4273d481c308a63c84f557749b6b80933b880
xhuve <xhuvelialbana@gmail.com>
2024-07-16 12:35:16 +0200

    Finish 3

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,25 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import csv
 import json
 
 app = Flask(__name__)
 
-@app.route('/<string:source>', defaults={'id': None})
-@app.route('/<string:source>/<string:id>')
-def hello(source, id):
+@app.route('/products')
+def hello():
+    source = request.args.get("source")
     if source == "json":
         with open("./products.json") as f:
             parsed_data = json.load(f)
+            id = request.args.get("id")
             if id:
                 specificData = [value for value in parsed_data if int(id) == value.get("id", None)]
                 if not specificData:
                     return render_template("product_display.html", error="Product not found")
                 return render_template("product_display.html", allProducts=specificData)
-            else:
-                return render_template("product_display.html", allProducts=parsed_data)
+            return render_template("product_display.html", allProducts=parsed_data)
     elif source == "csv":
         with open("./products.csv", mode="r") as f:
             reader = csv.DictReader(f)
+            id = request.args.get("id")
             if id:
                 csvData = [value for value in reader if id == value.get("id", None)]
                 if not csvData:
